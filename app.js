09fbe3787c054ea225d8619058da9038306f481f
Lena Buhl <l-buhl@hotmail.com>
2018-03-15 17:34:21 +0100

    adding stuff for message
    15.03

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ function User (username, email) {
   this.username = username
   this.email = email
 }
+// message constructor
+function Message (text, date, User) {
+  this.text = text
+  this.createdAT = new Date()
+  this.User = User
+}
 
 var Chat = (function() {
   //Users
@@ -12,7 +18,6 @@ var Chat = (function() {
   let module = {}
 
   module.joinChat = function(user) {
-    console.log('user is trying to join', user)
 
     let userAlreadyConnected = false
 
@@ -27,14 +32,18 @@ var Chat = (function() {
     } else {
       //add the user
       users.push(user)
+      console.log('user is trying to join', user)
 
     }
+  }
+
     module.sendChat = function (message) {
 //
       messages.push(message)
+      console.log("show all messages:", messages)
     }
 
-  }
+
 
   module.helloWorld = function () {
     console.log('Hello World')
@@ -49,7 +58,40 @@ var Chat = (function() {
 
 })()
 
-/*let user1 = new User("name", "email@asd.se");
+
+//FOR TESTING:
+let user1 = new User("name", "email@asd.se");
 let user2 = new User("name", "email@asd.se");
 
-Chat.joinChat(user1) */
+Chat.joinChat(user1)
+
+let message1 = new Message("Testing if it works", "15.03.18", user1);
+Chat.sendChat(message1)
+//console.log(messages); --> doesn't work because the array is declared w/block scope
+
+
+
+
+
+//***NOTES: SEARCHING***
+//as the drinks of a specific price search
+
+function ChatMessage(message, user) {
+    this.createdAT = new Date()
+}
+
+//search function in the module
+//and then:
+let keyword = "2"       //is case sensitive!
+
+let results = messages.filter(m => {
+  //if the current message matches what we are looking for
+  return m.message == keyword
+  //in order for it to searcg for a part of the message, not only containing 2, but more:
+  return m.message.indexOf(keyword) !== -1  //-1 for when there is no match - not true
+                      //when ever this returns true it means it's matching the keyword
+
+        //if we want it to be not case sensitive you can do it differently
+        //to begin with this is fine!   We could use a search method
+
+} )
